ec0b87f0beb8b11fa7419545ebdbfcc1249c864b
Jarek Šedý <jareksedy@icloud.com>
2022-03-19 09:14:03 +0600

    Structuring project.

---
+++ YSGBAppleMaps/Scenes/MapsScene/MapsScenePresenter.swift
@@ -0,0 +1,15 @@
+//
+//  MapsScenePresenter.swift
+//  YSGBAppleMaps
+//
+//  Created by Ярослав on 19.03.2022.
+//
+
+import UIKit
+
+// MARK: - Presenter
+final class MapsScenePresenter {
+    weak var viewDelegate: MapsSceneViewDelegate?
+    
+    // MARK: - Public methods
+}
---
+++ YSGBAppleMaps/Scenes/MapsScene/MapsSceneViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MapsSceneViewController.swift
+//  YSGBAppleMaps
+//
+//  Created by Ярослав on 19.03.2022.
+//
+
+import UIKit
+
+// MARK: - Protocol
+protocol MapsSceneViewDelegate: NSObjectProtocol {
+}
+
+// MARK: - Implementation
+extension MapsSceneViewController: MapsSceneViewDelegate {
+}
+
+// MARK: - Additional extensions
+
+// MARK: - View controller
+class MapsSceneViewController: UIViewController {
+    lazy var presenter = MapsScenePresenter()
+    
+    // MARK: - Methods
+    private func setupUI() {
+    }
+
+    // MARK: - Outlets
+    
+    // MARK: - Actions
+    
+    // MARK: - Selectors
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.viewDelegate = self
+        setupUI()
+    }
+}
